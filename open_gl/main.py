979497c33ec3edb2f07e46137e9b5a1739228ae8
Ageneh <henock.a@hotmail.de>
2019-06-19 00:41:27 +0200

    Added exit on insufficient arguments

--- open_gl/main.py
+++ open_gl/main.py
@@ -3,9 +3,17 @@ from sys import argv
 from open_gl.OBJParser import OBJParser
 from open_gl.RenderWindow import RenderWindow
 
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+
+
+def err():
+    print FAIL + "\nNo object found!\nPlease pass filepath of obj-file as argument.\n" + ENDC
+    exit(1)
+
+
 if __name__ == '__main__':
-    fpath = argv[1] if len(argv) > 2 else "./objects/cow.obj"
-    fpath = argv[1] if len(argv) > 2 else "./objects/squirrel.obj"
+    fpath = argv[1] if len(argv) > 2 else err()
 
     objparser = OBJParser(fpath)
     objparser.parse()
